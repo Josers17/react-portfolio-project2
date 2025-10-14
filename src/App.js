67f0a0684d61a3494a8e9e290da226195982054a
Jose Morales <josea.91@hotmail.com>
2025-10-14 14:26:03 -0500

    Add Button component (Click and Restart Button)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 import freeCodeCampLogo from './images/freecodecamp-logo.png';
+import Button from './components/Button';
 
 function App() {
+
+  const handleClick = () => {
+    console.log('Clic');
+  }
+
+  const restartCounter = () => {
+    console.log('Restart');
+  } 
+
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-container'>
@@ -11,7 +22,16 @@ function App() {
           alt='Logo of freeCodeCamp'/>
       </div>
       <div className='main-container'>
-
+        <Button
+          isButtonClick={true}
+          text='Click'
+          handleClick={handleClick}>
+        </Button>
+        <Button
+          isButtonClick={false}
+          text='Restart'
+          handleClick={restartCounter}>
+        </Button>
       </div>
     </div>
   );
