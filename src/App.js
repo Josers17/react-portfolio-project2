1e5e4c05ec30ba140fd7d9ec82f87552156d9c7d
Jose Morales <josea.91@hotmail.com>
2025-10-14 13:25:32 -0500

    Add logo to the counter and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
+      <div className='freecodecamp-logo-container'>
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo of freeCodeCamp'/>
+      </div>
+      <div className='main-container'>
+
+      </div>
     </div>
   );
 }
